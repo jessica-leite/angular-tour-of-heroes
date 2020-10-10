87ed9a456207af3e039b3d9c3f3060683b915c24
Jessica Leite <jessica.mleite@gmail.com>
2020-10-10 15:19:33 -0300

    Hero detail route

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,9 @@ export class HeroService {
     this.messageService.add('HeroService: fetched heroes ☻');
     return of(HEROES);
   }
+
+  getHero(id: number) {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
